03cf1565c5c1a650c83958f75fd6b09458f576ae
Damon <Damon@DESKTOP-JS2682A>
2018-04-21 21:10:51 +0100

    setup

---
+++ src/DirectoryHandle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace dir
+{
+    class DirectoryHandle
+    {
+        private string dir;
+        public DirectoryHandle()
+        {
+            setDefaultDir(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments));
+        }
+
+        public String[] getDirContents()
+        {
+
+           return Directory.GetFiles(dir);
+            
+        }
+
+
+
+       public string getDefaultDir()
+        {
+           return this.dir;
+        }
+
+        public void setDefaultDir(string dir)
+        {
+            this.dir = dir;
+        }
+    }
+}
---
+++ src/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace C_sharp_Compressor
+{
+    public partial class Form1 : Form
+    {
+        dir.DirectoryHandle directoryHandle = new dir.DirectoryHandle();
+        public Form1()
+        {
+            InitializeComponent();
+            Directory.
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String[] contents = directoryHandle.getDirContents();
+
+            foreach (var item in contents)
+            {
+                listBox1.Items.Add(item);
+            }
+        }
+    }
+}
